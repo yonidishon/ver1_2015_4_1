f2e523c97fc52c0bb7eb6674ffe7d7eea87373f8
yonidishon <ydishon@CGM10.ef.technion.ac.il>
2015-12-21 18:05:02 +0200

    adding forgotten files

---
+++ toolbox/pose_estimation/Visualization/vis_pos_train_BB.m
@@ -0,0 +1,68 @@
+% TODO - THIS SCRIPT ISN'T WORKING!!!
+% NOT COMPLETED
+% script for visualization of the foreground region of the train/test set
+% on the DIEM
+
+
+gaze = '\\cgm10\D\DIEM\gaze';
+movie_list = importdata('\\cgm10\D\DIEM\list.txt');
+%borji_list_subset = [6,8,10,11,12,14,15,16,34,42,44,48,53,54,55,59,70,74,83,84]; % Used by Borji on DIEM
+my_training_set = [7,18,20,21,22,32,39,41,46,51,56,60,65,72,73]; %15 vids - hand selected
+
+%my_training_set = borji_list_subset;
+train_set = movie_list(my_training_set);
+src_folder = '\\cgm10\D\head_pose_estimation\DIEMPCApng';
+BB_folder ='\\cgm10\D\head_pose_estimation\2015_19_12_new_train_form';
+addpath(genpath('\\cgm10\Users\ydishon\Documents\Video_Saliency\Dimarudoy_saliency\Dropbox\Matlab\video_attention'));
+addpath(genpath('\\cgm10\Users\ydishon\Documents\Video_Saliency\toolbox\pose_estimation'));
+NUMIMAGES=-1;% -1 = all in the dataset %100;
+SKIP =1;%5;
+OFFSET = 30;% to be consistent with Dmitry.
+totnumframes=0;
+
+pos_file = dir(fullfile(BB_folder,'train_pos*'));
+neg_file = dir(fullfile(BB_folder,'train_neg*'));
+if (isempty(pos_file)||isempty(neg_file))
+    error('couldn''t find input files');
+end
+pos_file = pos_file.name;
+neg_file = neg_file.name;
+posid = fopen(fullfile(BB_folder,pos_file));
+negid = fopen(fullfile(BB_folder,neg_file));
+tline = fgetl(posid);
+for k=1:length(train_set);
+    movie_name_no_ext = train_set{k};
+%     wobj = VideoWriter(fullfile(dst_folder,[movie_name_no_ext,'.avi']),'Uncompressed AVI');
+%     open(wobj);
+    gazeinfo=importdata(fullfile(gaze,[movie_name_no_ext,'.mat']),'data');
+    POS_BB = 
+    for ii=OFFSET:SKIP:length(gazeinfo.points);
+        fix_points = gazeinfo.points{ii};
+        att_map=points2GaussMap(fix_points',ones(1,length(fix_points)),0,[gazeinfo.width,gazeinfo.height],gazeinfo.pointSigma);
+        %[~,BB_POS]=patch_extract([gazeinfo.width,gazeinfo.height],gazeinfo.points{ii},gazeinfo.pointSigma,0);
+        th_att_map=att_map>=HIGHTH;
+        ind = find(repmat(th_att_map,1,1,3));
+%         BB_pos=regionprops(th_att_map,'BoundingBox');
+%         if isempty(BB_POS)
+%             continue;
+%         end
+%         if size(BB_pos,1)>1; % more than one positive position
+%             areas=regionprops(th_att_map,'Area');
+%             [areas,idx]=sort(extractfield(areas,'Area'));
+%             if areas(1) > 2*areas(2)
+%                 BB_pos = BB_pos.BoundingBox(idx(1));
+%             else
+%                 BB_pos = extractfield(BB_pos,'BoundingBox');
+%                 BB_pos = reshape(BB_pos,4,numel(areas));
+%             end
+%          end
+        f=imread(fullfile(src_folder,train_set{k},sprintf('%06d.png',ii)));
+        gray=repmat(round(rgb2gray(f)/2),1,1,3);
+        gray(ind)=f(ind);
+%        f=insertShape(f,'Circle',[BB_POS(5:6)+1,20;BB_POS(5:6)+1,0],'LineWidth',1);
+        writeVideo(wobj,gray);
+    end
+    close(wobj);
+    t= datetime('now','TimeZone','local','Format','d-MMM-y HH:mm:ss');
+    fprintf('%s: Finish processing #%i/%i movie:%s\n',datestr(t),k,length(train_set),movie_name_no_ext);
+end
